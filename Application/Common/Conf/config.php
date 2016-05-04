2f0f6fc81f59dc42dba59d09327195939e914817
ljw <myshenzhou@email.com>
2016-05-04 22:55:18 +0800

    think php go

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,26 @@
 <?php
+require("/myConfig.php");
 return array(
 	//'配置项'=>'配置值'
+	// URL请求地址的模式,0：普通 1：/模式 2：去掉index.php的/模式（需要REWRITE重写技术） 3：index.php?s=/xxx/xxx/xxx这种形式
+	'URL_MODEL' => 2,
+	//'URL_PATHINFO_DEPR' => '-',
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => $mysql_config['host'],
+	'DB_NAME' => 'wx_duinishuo',
+	'DB_USER' => $mysql_config['user'],
+	'DB_PWD' => $mysql_config['password'],
+	//'DB_PREFIX' => 'sh_',
+	/********* 默认请求的地址 ***********/
+	'DEFAULT_MODULE' => 'Home',
+	"DEFAULT_CONTROLLER" => "Index",
+	//接收到的数据使用函数过滤一下，就用于I函数
+	'DEFAULT_FILTER' => 'trim,htmlspecialchars',
+	//图片相关配置（自定义的，TP中没有）
+	'MAX_UPLOAD_FILESIZE' =>"3M",//允许上传的大最尺寸
+	"ALLOW_UPLOAD_FILETYPE" =>array('gif','png','jpg','jpeg','ejpeg','bmp'),
+	'ROOT_PATH'=>'./Public/Uploads/',//图片在硬盘上的路径-->给PHP程序用的
+	'VIEW_ROOT_PATH' => '/Public/Uploads/',//图片在网站中的路径-->给HTML浏览器用的
+	//md5加密的密钥
+	'MD5_KEY' => '!343!129fd$fd_fds=+43>?lg',
 );
--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -1,8 +1,11 @@
 <?php
-namespace Home\Controller;
+namespace Wechat\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	echo "这里是微信接口测试地址";
+    	$model=M("wx_token");
+    	
         $this->display();
     }
 }

e9ab77e46bd9ee630c9416ebf9915aa45b11a41a
ljw <654055148@qq.com>
2016-05-05 18:10:54 +0800

    path info

--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -5,6 +5,8 @@ class IndexController extends Controller {
     public function index(){
     	echo "这里是微信接口测试地址";
     	$model=M("wx_token");
+    	var_dump($model);
+    	echo pathinfo();
     	
         $this->display();
     }

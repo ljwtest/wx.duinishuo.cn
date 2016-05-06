08aa8743e1a6e70cc575fa602aad5ba9e9ba6d7e
ljw <654055148@qq.com>
2016-05-06 16:10:28 +0800

    test globals

--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -5,7 +5,7 @@ class IndexController extends Controller {
     public function index(){
     	echo "这里是微信接口测试地址";
     	$model=M("wx_token"); 	
-    	echo "test $GLOBALS["HTTP_RAW_POST_DATA"]";
+    	echo "test $GLOBALS['HTTP_RAW_POST_DATA']";
         $this->display();
     }
 }

92328b864e0647cf568dce3d03144f353d12c3e6
ljw <654055148@qq.com>
2016-05-06 14:29:48 +0800

    124124124

--- Application/Wechat/Controller/IndexController.class.php
+++ Application/Wechat/Controller/IndexController.class.php
@@ -4,14 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
     	echo "这里是微信接口测试地址";
-    	$model=M("wx_token");
-    	//var_dump($model);
-    	var_dump($_SERVER['PATH_INFO']);
-    	var_dump(pathinfo($_SERVER['PATH_INFO']));
-    	echo "<hr>";
-    	var_dump($_GET);
-		echo "测试git是否能同步,第二次测试，测试php:input"; 
-		phpinfo();   	
+    	$model=M("wx_token"); 	
         $this->display();
     }
 }

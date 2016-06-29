068de3357eee471f1018156e5a7611693b5ad83f
laijun <xxxxx@email.com>
2016-06-30 04:57:24 +0800

    a

--- st.php
+++ st.php
@@ -8,7 +8,7 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=0.5">
 <title>要不留下来，要不你跟我走</title>
 
 <link rel="stylesheet" href="css/reset.css">

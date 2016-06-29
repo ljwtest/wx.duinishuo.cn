dc8e9ed76492e72652d13343a697264231dd7eaa
laijun <xxxxx@email.com>
2016-06-30 04:50:53 +0800

    a

--- st.php
+++ st.php
@@ -6,7 +6,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=gbk2312" />
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>要不留下来，要不你跟我走</title>
 
 <link rel="stylesheet" href="css/reset.css">
